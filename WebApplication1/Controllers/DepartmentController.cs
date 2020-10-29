4e5ce35496df058ca6dce30a841a1264b5d90d68
Sasha Sheydakov <alsheyda@microsoft.com>
2020-10-29 13:12:13 +0000

    Add project files.

---
+++ WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"SELECT DepartmentID, DepartmentName FROM dbo.Department";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table); 
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+        public HttpResponseMessage Post(Department)
+        {
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+}
